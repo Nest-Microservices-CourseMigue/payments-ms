e491f7e0b0df5171f3358dd6bca4e63dd4756c3b
MiguelGH6586 <miguel.agh97@gmail.com>
2024-11-21 22:19:59 -0500

    payment microservice created

---
+++ src/payments/dto/payment-session.dto.ts
@@ -0,0 +1,36 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNumber,
+  IsPositive,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+export class PaymentSessionDto {
+  @IsString()
+  orderId: string;
+
+  @IsString()
+  currency: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => PaymentaSessionItemDto)
+  items: PaymentaSessionItemDto[];
+}
+
+export class PaymentaSessionItemDto {
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsNumber()
+  @IsPositive()
+  quantity: number;
+}
